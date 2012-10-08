cced4018a9d27fee7004a025c06a675e617faad6
Wikzo <Linkthewindwaker@gmail.com>
2012-10-08 17:36:51 +0200

    Now the edge detection is working. Still need the Median filter with uchar array

--- MyOpenCV/OpenCV_test3/Image.cpp
+++ MyOpenCV/OpenCV_test3/Image.cpp
@@ -4,66 +4,73 @@
 using namespace cv;
 using namespace std;
 
+enum SobelDirection
+{
+	Diagonal_Right,
+	Diagonal_Left,
+	Vertical,
+	Horizontal
+};
+
+const int THRESHOLD_VALUE_FOR_BUILDING_IMAGE = 133; // optimal value was found using ImageJ
+
 Mat ConvertColorImageToBlackWhite(Mat colorImage);
-Mat ThresholdBlackWhiteImage(Mat blackWhiteImage);
-Mat EdgeDetecting(Mat inputImage);
+Mat MeanFilter(Mat input);
+Mat ThresholdBlackWhiteImage(Mat blackWhiteImage, int threshold);
+Mat SobelEdgeDetecting(Mat input, enum SobelDirection direction, bool useMeanFilterBeforeDoingEdgeDetecting, int threshold);
+Mat AddTwoMatsTogether(Mat matA, Mat matB);
+void SortValues(uchar* a, int size);
+
+
 
 int main()
 {
-	Mat colorImage;
-	Mat gray;
-
-	// Use image
-    /*colorImage = imread("chrome.jpg");
+	cout << "Edge detection using the Sobel kernel (and OpenCV to load images)" << endl;
+	cout << "By Gustav Dahl - Medialogy 3rd semester 2012 - Aalborg University\n\n";
+	// Load the image image
+    Mat colorImage = imread("building.jpg");
     if (colorImage.empty()) 
     {
         cout << "Cannot load image!" << endl;
         return -1;
-    }*/
+    }
 
-
-	// Use camera instead of picture
-	// -----------------------------------------------------
-	VideoCapture capture;
-	capture.open(0);
-	//capture.set(CV_CAP_PROP_FPS, 15);
-
-	while (true)
-	{
-		capture >> colorImage;
-		if (colorImage.empty())
-			break;
-
-		gray = ConvertColorImageToBlackWhite(colorImage);
-		
-		imshow("videoColor", colorImage);
-		imshow("videoGray", gray);
-
-		if ((char)waitKey(1) == 'q')
-			break;
-	}
-
-	// -----------------------------------------------------
+	cout << "Processing image. Please wait..." << endl;
 
 	// Convert to black-white
-	//Mat gray = ConvertColorImageToBlackWhite(colorImage);
-	//cv::imwrite("chrome_grayscale.jpg", gray);
+	Mat gray = ConvertColorImageToBlackWhite(colorImage);
+	cv::imwrite("grayscale.jpg", gray);
+
+	// Mean (black and white only)
+	Mat mean = MeanFilter(gray);
+	imwrite("mean.jpg", mean);
 
 	// Threshold
-	//Mat threshold = ThresholdBlackWhiteImage(gray);
+	Mat threshold = ThresholdBlackWhiteImage(gray, THRESHOLD_VALUE_FOR_BUILDING_IMAGE);
+	imwrite("threshold.jpg", threshold);
 
 	// Edge detecting
-	//Mat edge = EdgeDetecting(threshold);
+	Mat edge_diagonal_right = SobelEdgeDetecting(gray, Diagonal_Right, true, THRESHOLD_VALUE_FOR_BUILDING_IMAGE);
+	Mat edge_diagonal_left = SobelEdgeDetecting(gray, Diagonal_Left, true, THRESHOLD_VALUE_FOR_BUILDING_IMAGE);
+	Mat edge_vertical = SobelEdgeDetecting(gray, Vertical, true, THRESHOLD_VALUE_FOR_BUILDING_IMAGE);
+	Mat edge_horizontal = SobelEdgeDetecting(gray, Horizontal, true, THRESHOLD_VALUE_FOR_BUILDING_IMAGE);
 
-	// Edge detecting (OpenCV)
-	//cv::Canny(gray, edge, 100, 250);
+	Mat verti_plus_horiz = AddTwoMatsTogether(edge_vertical, edge_horizontal);
+	Mat diagonal_right_plus_left = AddTwoMatsTogether(edge_diagonal_right, edge_diagonal_left);
+	Mat diagonal_plus_vertical_horizontal = AddTwoMatsTogether(verti_plus_horiz, diagonal_right_plus_left);
 
-    //cv::imshow("Color image", colorImage);
-	//cv::imshow("Grayscale image", gray);
-	//cv::imshow("Binary image", threshold);
-	//cv::imshow("Edge", edge);
+	imwrite("diagonal_plus_vertical_horizontal.jpg", diagonal_plus_vertical_horizontal);
 
-	//cv::imshow("Edge", edge);
+
+    cv::imshow("Original image", colorImage);
+	cv::imshow("Grayscale image", gray);
+	cv::imshow("Mean filter", mean);
+	cv::imshow("Binary image", threshold);
+	cv::imshow("Edge diagonal right", edge_diagonal_right);
+	cv::imshow("Edge diagonal left", edge_diagonal_left);
+	cv::imshow("Edge vertical", edge_vertical);
+	cv::imshow("Edge horizontal", edge_horizontal);
+	cv::imshow("Diagonal plus vertical and horizontal", diagonal_plus_vertical_horizontal);
 
     waitKey(0);
 }
@@ -73,15 +80,8 @@ Mat ConvertColorImageToBlackWhite(Mat colorImage)
 	// Mat(rows, columns, type)
 	// rows = y = 300
 	// cols = x = 600
-	Mat grayScaleImage(colorImage.rows, colorImage.cols, CV_8UC1);
 
-	// Check color image size and channels
-	//cout << "colorImage rows: " << colorImage.rows << "; colorImage cols: " << colorImage.cols << endl;
-	//cout << "colorImage channels: " << colorImage.channels() << endl;
-
-	// Check grayscale image size and channels
-	//cout << "\ngrayScaleImage rows: " << grayScaleImage.rows << "; grayScaleImage cols: " << grayScaleImage.cols << endl;
-	//cout << "grayScaleImage channels: " << grayScaleImage.channels() << endl;
+	Mat grayScaleImage(colorImage.rows, colorImage.cols, CV_8UC1); // new image with only 1 channel
 
 	// Formula for converting from color to grayscale (3.3, p. 30 in Introduction to Video and Image Processing book)
 	// I = weightR * R + weightG * G + weightB * B
@@ -91,121 +91,272 @@ Mat ConvertColorImageToBlackWhite(Mat colorImage)
 	float GreenWeight = 0.587;
 	float BlueWeight = 0.114;
 	
-	//int yTemp = 199; // 299 = max
-	//int xTemp = 299; // 599 = max
-	//cout << endl << endl;
-	//Vec3b val = colorImage.at<Vec3b>(yTemp, xTemp)[2];
-	//cout << "val at [" << yTemp << ", " << xTemp << "]: " << (int)val(0) << endl;
-	//rectangle(colorImage, Point(xTemp, yTemp), Point(xTemp+10, yTemp+10), CV_RGB(255, 0, 0), 3);
 
-
-	//Iterate through all the pixels and apply the formula for grayscale
+	// Iterate through all the pixels and apply the formula for grayscale
 	for (int y = 0; y < colorImage.rows; y++) // rows
 	{
-		for (int x = 0; x < colorImage.cols; x++) // < ------------------------------ CANNOT BE BIGGER THAN 200!?!?
+		for (int x = 0; x < colorImage.cols; x++)
 		{
 			// Calculate grayscale value
 			int grayValue = colorImage.at<cv::Vec3b>(y, x)[0] * BlueWeight
 				+ colorImage.at<cv::Vec3b>(y, x)[1] * GreenWeight
 				+ colorImage.at<cv::Vec3b>(y, x)[2] * RedWeight;
 
-			// Print values
-			//cout << "\n\n-------------\nx: " << x << " and y: " << y << endl;
-			//cout << "grayValue: " << grayValue << endl;
-			grayScaleImage.at<uchar>(y, x) = grayValue;
-
-			//Vec3b val = grayScaleImage.at<Vec3b>(y, x);
-			//cout << "Actual val: " << (int)val(0) << endl; // <------------- WORKS!!			
+			// Apply the grayscale value (0-255)
+			grayScaleImage.at<uchar>(y, x) = grayValue;		
 
 		}
 	}	
 	return grayScaleImage;
 }
 
-Mat ThresholdBlackWhiteImage(Mat blackWhiteImage)
+Mat MeanFilter(Mat input)
+{
+	// 3x3 kernel size
+
+	Mat mean = input.clone();
+
+	// Loop through all pixels
+	for (int y = 0; y < input.rows-2; y++)
+	{
+		for (int x = 0; x < input.cols-2; x++)
+		{
+			if (x - 2 < 0 || y - 2 < 0) // don't go out of bounds
+				continue;
+
+			mean.at<uchar>(y, x) = (
+				input.at<uchar>(y-2, x-2) + input.at<uchar>(y-2, x-1)
+				+ input.at<uchar>(y-2, x) + input.at<uchar>(y-2, x+1)
+				+ input.at<uchar>(y-2, x+2) + input.at<uchar>(y-1, x-2)
+				+ input.at<uchar>(y-1, x-1) + input.at<uchar>(y-1, x)
+				+ input.at<uchar>(y-1, x+1) + input.at<uchar>(y-1, x+2)
+				+ input.at<uchar>(y, x-2) + input.at<uchar>(y, x-1)
+				+ input.at<uchar>(y, x) + input.at<uchar>(y, x+1)
+				+ input.at<uchar>(y, x+2) + input.at<uchar>(y+1, x-2)
+				+ input.at<uchar>(y+1, x-1) + input.at<uchar>(y+1, x)
+				+ input.at<uchar>(y+1, x+1) + input.at<uchar>(y+1, x+2)
+				+ input.at<uchar>(y+2, x-2)	+ input.at<uchar>(y+2, x-1)
+				+ input.at<uchar>(y+2, x) + input.at<uchar>(y+2, x+1)
+				+ input.at<uchar>(y+2, x+2)
+				) / 25;
+		}
+	}
+
+	return mean;
+}
+
+Mat ThresholdBlackWhiteImage(Mat blackWhiteImage, int threshold)
 {
 	Mat image = blackWhiteImage.clone();
-	int threshold = 177;
 
+	// Loop through all pixels and set them to either 255 (white) or 0 (black) using the threhold value
 	for (int y = 0; y < image.rows; y++)
 	{
 		for (int x = 0; x < image.cols; x++)
 		{
-			float temp = image.at<cv::Vec3b>(y, x)[0];
-
-			if (temp >= threshold)
-			{
-				image.at<cv::Vec3b>(y, x)[0] = 255;
-				image.at<cv::Vec3b>(y, x)[1] = 255;
-				image.at<cv::Vec3b>(y, x)[2] = 255;
-				
-			}
+			if (image.at<uchar>(y, x) <= threshold)
+				image.at<uchar>(y, x) = 255;
 			else
-			{
-				image.at<cv::Vec3b>(y, x)[0] = 0;
-				image.at<cv::Vec3b>(y, x)[1] = 0;
-				image.at<cv::Vec3b>(y, x)[2] = 0;
-			}
+				image.at<uchar>(y, x) = 0;
 		}
 	}
 
 	return image;
 }
 
-Mat EdgeDetecting(Mat inputImage)
+Mat SobelEdgeDetecting(Mat input, enum SobelDirection direction, bool useMeanFilterBeforeDoingEdgeDetecting, int threshold)
 {
-	Mat image = inputImage.clone();
+	Mat edge = input.clone();
 
-	int xElements = image.cols;
-	int yElements = image.rows;
+	if (useMeanFilterBeforeDoingEdgeDetecting)
+		edge = MeanFilter(edge);
 
-	int pixelThreshold = 5;
-
-	bool* myArrayX = new bool[xElements];
-	bool* myArrayY = new bool[yElements];
-
-	for (int i = 0; i < xElements; i++)
-		myArrayX[i] = false;
-
-	for (int i = 0; i < yElements; i++)
-		myArrayY[i] = false;
-
-	for (int y = 0; y < image.rows; y++)
+	// Apply diagonal edge detecting RIGHT
+	if (direction == Diagonal_Right)
 	{
-		for (int x = pixelThreshold; x < 250; x++)
+		for (int y = 0; y < input.rows-1; y++)
 		{
-			// We have an edge
-			/*if ((image.at<cv::Vec3b>(y, x)[0] != image.at<cv::Vec3b>(y, x-pixelThreshold)[0]) && (image.at<cv::Vec3b>(y, x-pixelThreshold)[0] != image.at<cv::Vec3b>(y, x+pixelThreshold)[0]))
+			for (int x = 0; x < input.cols-1; x++)
 			{
-				myArrayX[x] = true;
-				myArrayY[y] = true;
-			}*/
+				if (x-1 < 0 || y-1 < 0) // don't go out of bounds
+					continue;
 
-			// We have an edge
-			if ((image.at<cv::Vec3b>(y, x)[0] != image.at<cv::Vec3b>(y, x+1)[0]))
-			{
-				image.at<cv::Vec3b>(x, x)[0] = 20;
-				image.at<cv::Vec3b>(x, x)[1] = 20;
-				image.at<cv::Vec3b>(x, x)[2] = 200;
+				edge.at<uchar>(y, x) = (
+					(input.at<uchar>(y-1, x-1)) * -2
+					+ (input.at<uchar>(y, x-1)) * -1
+					+ (input.at<uchar>(y+1, x-1)) * 0
+					+ (input.at<uchar>(y-1, x)) * -1
+					+ (input.at<uchar>(y, x)) * 0
+					+ (input.at<uchar>(y+1, x+0)) * 1
+					+ (input.at<uchar>(y-1, x+1)) * 0
+					+ (input.at<uchar>(y, x+1)) * 1
+					+ (input.at<uchar>(y+1, x+1)) * 2
+					);
 			}
 		}
 	}
-
-	// Put red color in
-	for (int y = 0; y < 500;)
-	{
-		y++;
-		for (int x = 0; x < 200;)
+	else if (direction == Diagonal_Left)
+	{	// Apply diagonal edge detecting LEFT
+		for (int y = 0; y < input.rows-1; y++)
 		{
-			x++;
-			if (myArrayX[x] == true)
+			for (int x = 0; x < input.cols-1; x++)
 			{
-				image.at<cv::Vec3b>(x, x)[0] = 20;
-				image.at<cv::Vec3b>(x, x)[1] = 20;
-				image.at<cv::Vec3b>(x, x)[2] = 200;
+				if (x-1 < 0 || y-1 < 0) // don't go out of bounds
+					continue;
+
+				edge.at<uchar>(y, x) = (
+					(input.at<uchar>(y-1, x-1)) * -2
+					+ (input.at<uchar>(y, x-1)) * -1
+					+ (input.at<uchar>(y+1, x-1)) * 0
+					+ (input.at<uchar>(y-1, x)) * -1
+					+ (input.at<uchar>(y, x)) * 0
+					+ (input.at<uchar>(y+1, x+0)) * 1
+					+ (input.at<uchar>(y-1, x+1)) * 0
+					+ (input.at<uchar>(y, x+1)) * 1
+					+ (input.at<uchar>(y+1, x+1)) * 2
+					);
 			}
 		}
 	}
+	else if (direction == Vertical)
+	{
+		// Apply diagonal edge detecting ´VERTICAL
+		for (int y = 0; y < input.rows-1; y++)
+		{
+			for (int x = 0; x < input.cols-1; x++)
+			{
+				if (x-1 < 0 || y-1 < 0) // don't go out of bounds
+					continue;
 
-	return image;
+				edge.at<uchar>(y, x) = (
+					(input.at<uchar>(y-1, x-1)) * -1
+					+ (input.at<uchar>(y, x-1)) * -2
+					+ (input.at<uchar>(y+1, x-1)) * -1
+					+ (input.at<uchar>(y-1, x)) * -0
+					+ (input.at<uchar>(y, x)) * 0
+					+ (input.at<uchar>(y+1, x+0)) * 0
+					+ (input.at<uchar>(y-1, x+1)) * 1
+					+ (input.at<uchar>(y, x+1)) * 2
+					+ (input.at<uchar>(y+1, x+1)) * 1
+					);
+			}
+		}
+	}
+	else if (direction == Horizontal)
+	{
+		// Apply diagonal edge detecting HORIZONTAL
+		for (int y = 0; y < input.rows-1; y++)
+		{
+			for (int x = 0; x < input.cols-1; x++)
+			{
+				if (x-1 < 0 || y-1 < 0) // don't go out of bounds
+					continue;
+
+				edge.at<uchar>(y, x) = (
+					(input.at<uchar>(y-1, x-1)) * -1
+					+ (input.at<uchar>(y, x-1)) * 0
+					+ (input.at<uchar>(y+1, x-1)) * 1
+					+ (input.at<uchar>(y-1, x)) * -2
+					+ (input.at<uchar>(y, x)) * 0
+					+ (input.at<uchar>(y+1, x+0)) * 2
+					+ (input.at<uchar>(y-1, x+1)) * -1
+					+ (input.at<uchar>(y, x+1)) * 0
+					+ (input.at<uchar>(y+1, x+1)) * 1
+					);
+			}
+		}
+	}
+	else
+	{
+		// Error text
+		putText(edge, "ERROR - Sobel type not defined!", Point(10, 50), FONT_HERSHEY_PLAIN, 2, Scalar(0, 0, 255), 4, 8, false);
+		putText(edge, "ERROR - Sobel type not defined!", Point(10, 150), FONT_HERSHEY_PLAIN, 2, Scalar(0, 0, 255), 4, 8, false);
+		putText(edge, "ERROR - Sobel type not defined!", Point(10, 250), FONT_HERSHEY_PLAIN, 2, Scalar(0, 0, 255), 4, 8, false);
+		putText(edge, "ERROR - Sobel type not defined!", Point(10, 300), FONT_HERSHEY_PLAIN, 2, Scalar(0, 0, 0), 4, 8, false);
+		putText(edge, "ERROR - Sobel type not defined!", Point(10, 450), FONT_HERSHEY_PLAIN, 2, Scalar(0, 0, 0), 4, 8, false);
+		putText(edge, "ERROR - Sobel type not defined!", Point(10, 600), FONT_HERSHEY_PLAIN, 2, Scalar(0, 0, 0), 4, 8, false);
+	}
+
+	// Threshold
+	edge = ThresholdBlackWhiteImage(edge, threshold);
+	return edge;
 }
+
+Mat AddTwoMatsTogether(Mat matA, Mat matB) // should be same size!
+{
+	Mat output = matA.clone();
+
+	for (int y = 0; y < matA.rows; y++)
+	{
+		for (int x = 0; x < matA.cols; x++)
+		{
+			output.at<uchar>(y, x) = matA.at<uchar>(y, x) + matB.at<uchar>(y, x);
+		}
+
+	}
+
+	output = ThresholdBlackWhiteImage(output, THRESHOLD_VALUE_FOR_BUILDING_IMAGE);
+	return output;
+}
+
+// Not working yet
+/*void MedianFilter(Mat input, int kernelSize)
+{
+	if (kernelSize % 2 == 0) // don't use even numbers
+		kernelSize++;
+
+	int radius = kernelSize / 2; // e.g. kernel is 7X7 --> radius is 3
+
+	uchar neighborPixels[9];
+
+	for (int y = radius; radius < (input.rows - radius); y++)
+	{
+		int counter = 0;
+		for (int x = radius; x < (input.cols - radius); x++)
+		{
+			// Done
+			if (counter >= 9)
+			{
+				//start sort
+				// put pixels back in
+			}
+			neighborPixels[x*y] = input.at<uchar>(y, x);
+			counter++;
+		}
+	}
+}*/
+
+// Not working yet
+/*uchar* SortValues(uchar* a, int size)
+{
+	cout << "Before the bubblesort:" << endl;
+	for (int i = 0; i < size; i++)
+		cout << a[i] << endl;
+
+	// Bubble list
+	bool swapped = true;
+
+	while (swapped)
+	{
+		swapped = false;
+		for (int i = 1; i < size; i++)
+		{
+			if (a[i-1] > a[i])
+			{
+				// Sort numbers
+				int temp = a[i-1];
+				a[i-1] = a[i];
+                a[i] = temp;
+
+                swapped = true;
+			}
+		}
+	}
+
+	cout << "After the bubblesort:" << endl;
+	for (int i = 0; i < size; i++)
+		cout << a[i] << endl;
+
+	uchar* aPointer = a;
+	//return aPointer;
+}*/
